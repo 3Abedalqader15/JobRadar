7504f74e17fe376976b95520fa9100f507ff56c9
Abood <abedelqaberalfaqeeh@gmail.com>
2026-09-23 00:29:16 +0300

    SmokeTest

--- src/JobRadar.Infrastructure/obj/Debug/net8.0/JobRadar.Infrastructure.AssemblyInfo.cs
+++ src/JobRadar.Infrastructure/obj/Debug/net8.0/JobRadar.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobRadar.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72de0d8ced22697aef06e0b8e24d53dd9cdbae02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+029808397160a473e338d4ed870c68f203edbffc")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobRadar.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobRadar.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/JobRadar.Application.UnitTests/obj/Debug/net10.0/JobRadar.Application.UnitTests.AssemblyInfo.cs
+++ tests/JobRadar.Application.UnitTests/obj/Debug/net10.0/JobRadar.Application.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobRadar.Application.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72de0d8ced22697aef06e0b8e24d53dd9cdbae02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+029808397160a473e338d4ed870c68f203edbffc")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobRadar.Application.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobRadar.Application.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
